ba9deed6d34c6958d65b35dfaf68ba2fb665142c
김재욱 <soo8848@naver.com>
2025-12-19 09:06:00 +0900

    fix: update jjwtToken nickname

--- src/main/java/com/zero/plantory/global/security/auth/controller/AuthRestController.java
+++ src/main/java/com/zero/plantory/global/security/auth/controller/AuthRestController.java
@@ -99,6 +99,7 @@ public class AuthRestController {
         return ResponseEntity.ok(Map.of(
                 "user", Map.of(
                         "memberId", member.getMemberId(),
+                        "nickname", member.getNickname(),
                         "membername", member.getMembername(),
                         "role", member.getRole()
                 ),
