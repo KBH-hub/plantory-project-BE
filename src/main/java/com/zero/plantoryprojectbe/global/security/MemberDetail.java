75b4675dfa526472fbbf7be1175abca65ed36f42
soo8848 <soo8848@naver.com>
2025-12-25 22:37:51 +0900

    feat: add MemberPrincipal

--- src/main/java/com/zero/plantoryprojectbe/global/security/MemberDetail.java
+++ src/main/java/com/zero/plantoryprojectbe/global/security/MemberDetail.java
@@ -26,4 +26,8 @@ public record MemberDetail(MemberResponse memberResponse) implements UserDetails
         return memberResponse.getMembername();
     }
 
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
 }
